29a8c775719a60187793bbc937699b1d3eeb4508
Dasu2313 <dasu23@mail.ru>
2025-05-20 21:29:37 +0500

    change link

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  base: 'https://dasu2313.github.io/todo-react/',
+  base: 'https://dasu2313.github.io/movies-react/',
   plugins: [react(), eslint()],
 })
