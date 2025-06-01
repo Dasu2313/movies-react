5bf294154f21d8249277ff6ac8a70227532a08f7
Dasu2313 <dasu23@mail.ru>
2025-06-01 23:49:16 +0500

    fix media

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -1,6 +1,10 @@
 const CardImage = (props) => {
   return (
-    <img style={{ width: '183px' }} src={`https://image.tmdb.org/t/p/w500${props.posterPath}`} />
+    <img
+      style={{ width: '183px' }}
+      src={`https://image.tmdb.org/t/p/w500${props.posterPath}`}
+      className="cardImg"
+    />
   )
 }
 
